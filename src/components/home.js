1f2d3ff0cc59165244e6d9ea62349585dad64db8
Evan Leong <evleong1@gmail.com>
2024-01-09 22:31:15 -0800

    refactor: skills.js mapping from data.js

--- src/components/home.js
+++ src/components/home.js
@@ -8,15 +8,15 @@ import { SlArrowDown } from "react-icons/sl";
 
 function Home () {
   return (
-    <div className="flex flex-col justify-center items-center w-full">
-      <div className="text-[#22610B] text-[7rem] font-UnbuntoMono font-extrabold flex flex-col justify-center items-center gap-0">
+    <div className="flex flex-col justify-center items-center w-full ">
+      <div className="text-[#22610B] text-9xl font-UnbuntoMono font-extrabold flex flex-col justify-center items-center gap-5">
         <p>Haley Khuu Inzunza</p>
         <div className='w-[90%] h-[5px] bg-[#22610B] rounded-full' />
       </div>
-      <div className='flex flex-row items-center justify-center gap-10 mt-8'>
-      <img src={pfp} className="w-[25%] hover:scale-105 transition" alt="Haley Icon" draggable={false}/>
+      <div className='flex flex-row items-center justify-center gap-10 mt-5 '>
+      <img src={pfp} className="w-[20%] hover:scale-105 transition" alt="Haley Icon" draggable={false}/>
       <div className=''>
-        <div className="text-3xl mt-2 text-left leading-relaxed font-UnbuntoMono">
+        <div className="text-2xl mt-2 text-left leading-relaxed font-UnbuntoMono">
             Hi! I'm Haley Inzunza, a software developer. I graduated
             <br/>
             with a degree in Computer Science from UC Irvine this past
@@ -38,15 +38,15 @@ function Home () {
         <div className="flex flex-row justify-center items-center mt-4 gap-7">
           <a href="https://github.com/haleeei">
               {/* <img src={github} alt="GitHub icon"/> */}
-                <FiGithub size={80} className='text-black active:text-gray-600 hover:scale-105 transition '/ >
+                <FiGithub size={60} className='text-black active:text-gray-600 hover:scale-105 transition '/ >
           </a>
           <a href="https://www.linkedin.com/in/haley-inzunza-76033914a/">
               {/* <img src={linkedIn} alt="Linkedin icon"/>*/}
-                <FaLinkedin size={80} className='text-black active:text-gray-600 hover:scale-105 transition '/>
+                <FaLinkedin size={60} className='text-black active:text-gray-600 hover:scale-105 transition '/>
           </a>
           <a href={resume}>
               {/* <img src={resume} alt="Resume icon"/> */}
-                <HiOutlineNewspaper size={90} className='text-black active:text-gray-600 hover:scale-105 transition '/>
+                <HiOutlineNewspaper size={80} className='text-black active:text-gray-600 hover:scale-105 transition '/>
           </a>
         </div>
       </div>
